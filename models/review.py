0a828b04efdb2a6ae8848bbc0bdc8ddedcc7502a
PrudenceLive <pmprue328@gmail.com>
2023-11-22 10:02:57 -0800

    Testing

--- models/review.py
+++ models/review.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project"""
-from base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
+""" Review module for the HBNB project """
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    if models.storage_t == 'db':
-        __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
+    __tablename__ = "reviews"
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
