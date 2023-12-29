4af86b6ebdc358e001344ebfc707aaba3c6377bb
charitylefosa <charitylefosa@gmail.com>
2023-12-29 03:54:27 -0800

    save

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,45 @@
 #!/usr/bin/python3
-"""Starts a Flask web application"""
+"""
+A script to to load all cities of a State,
+Ig storage engine is DBStorage, use cities relationship
+Otherwise, use the public getter method cities.
+After ech request remove the current method cities.
+Declareamethod to handle @app.trardown_appcontext
+Call in this method storage.close()
+Routes:
+/cities_by_states: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
 
+"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """A function that serves as the home route of the application.
+    Returns:
+        str: The greeting message "Hello HBNB!".
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays cities per state"""
+    states = storage.all(State)
+    return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Closes the storage at the end of the request"""
+def teardown_context(ctx):
+    """Displays cities per state"""
     storage.close()
 
 
-@app.route('/states_list')
-def states_list():
-    """Displays a list of states"""
-    states = storage.all("State").values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-
-    return render_template('7-states_list.html', states=sorted_states)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
